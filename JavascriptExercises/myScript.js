8b4ddfd2307e38f1d59de85ce1ea36d8eedccc93
Baudrit Corentin <baudritcorentin@hotmail.com>
2019-01-21 16:15:28 +0000

    Commit of the script exercises

---
+++ JavascriptExercises/myScript.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("block").innerHTML = "Whale Hello there !";
+}
+
+function myAddition(){
+    document.getElementById("demo").innerHTML = 5 + 6;
+}
