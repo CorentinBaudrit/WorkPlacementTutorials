895c7ddd53ba42d2b80f09c9dd1ac95117812def
Corentin Baudrit <corentin.baudrit@students.ittralee.ie>
2019-01-22 17:11:03 +0000

    Add Heroes angular tutorial project

---
+++ AngularFormation/myapp/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Deku' },
+  { id: 12, name: 'All Might' },
+  { id: 13, name: 'Bakugo' },
+  { id: 14, name: 'Froppy' },
+  { id: 15, name: 'All For one' },
+  { id: 16, name: 'Mount Lady' },
+  { id: 17, name: 'Overhaul' },
+  { id: 18, name: 'Red Riot' },
+  { id: 19, name: 'Earaser Head' },
+  { id: 20, name: 'Ectoplasm' }
+];
